47e426035a664a9c26bf00b0579db0363f575346
mohammed kadiri <mohammedkadiri72@gmail.com>
2021-02-10 19:01:29 +0000

    Implemented dynamic page for each manga detailing the corresponding information

--- app.py
+++ app.py
@@ -10,12 +10,23 @@ app.config['MYSQL_DB'] = 'manga'
 
 mysql = MySQL(app)
 
-def category(name):
+
+
+def getData(query):
     cur = mysql.connection.cursor()
-    query = 'select * from manga join manga_genre on manga_genre.manga_id = manga.ID join genre on genre.ID = manga_genre.genre_id where genre.Category =\'{0}\''.format(name)
     cur.execute(query)
     fetchdata = cur.fetchall()
     return fetchdata
+
+def categoryQuery(category):
+    query = 'select * from manga join manga_genre on manga_genre.manga_id = manga.ID join genre on genre.ID = manga_genre.genre_id where genre.Category =\'{0}\''.format(category)
+    return query
+
+def mangaQuery(name):
+    query = 'select * from manga where manga.Name = \'{0}\''.format(name)
+    return query
+
+
 
 
 @app.route('/')
@@ -25,20 +36,28 @@ def home():
 
 @app.route('/action.html')
 def action():
-    return render_template('action.html', data =  category('action'))
+    query = categoryQuery('action')
+    return render_template('action.html', data = getData(query))
 
 @app.route('/adventure.html')
 def adventure():
-    return render_template('adventure.html', data = category('adventure'))
+    query = categoryQuery('adventure')
+    return render_template('adventure.html', data = getData(query))
 
 @app.route('/comedy.html')
 def comedy():
-    return render_template('adventure.html', data = category('comedy'))
+    query = categoryQuery('comedy')
+    return render_template('adventure.html', data = getData(query))
 
 @app.route('/sport.html')
 def sport():
-    return render_template('sport.html', data = category('sport'))
+    query = categoryQuery('sport')
+    return render_template('sport.html', data = getData(query))
 
+@app.route('/<string:manga_name>')
+def manga(manga_name):
+    query = mangaQuery(manga_name)
+    return render_template('manga.html', data = getData(query))
 
 if __name__ == "__main__":
     app.run(debug=True)
